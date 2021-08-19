4547b8b7a1fa9785d56b99b871cbb982647cf10a
WrightChristian-FS <cmwright@student.fullsail.edu>
2021-08-19 14:04:49 -0700

    Completed Menu Class

---
+++ ATMApplication/App.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ATMApplication
+{
+    public class App
+    {
+        public App()
+        {
+
+            Console.WriteLine("App Connected");
+        }
+    }
+}
---
+++ ATMApplication/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ATMApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            App app = new App(); 
+        }
+    }
+}
